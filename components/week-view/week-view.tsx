9acc59316e5a2294008b707bb9984980de10b2fe
ninest <parth.kabra@gmail.com>
2023-02-17 12:29:47 -0500

    Add week calendar

--- components/week-view/week-view.tsx
+++ components/week-view/week-view.tsx
@@ -1,3 +1,44 @@
+import { TIME_HOURS, WEEK_DAYS } from "@/utils/date/display";
+
 export const WeekView = () => {
-  return <> WeekView</>;
+  return (
+    <>
+      <div
+        className="grid"
+        style={{ gridTemplateColumns: "3rem repeat(5, minmax(0, 1fr))" }}
+      >
+        {/* Alternate table row colors */}
+        {[...new Array(10)].map((_, i) => (
+          <div
+            key={i}
+            style={{
+              gridRow: `${12 * (i + 0.5) * 2 + 3} / span 12`,
+              gridColumn: "2 / span 8",
+            }}
+            className="bg-gray-50 rounded"
+          />
+        ))}
+        {/* Days */}
+        {WEEK_DAYS.map((day, index) => (
+          <div
+            key={index}
+            style={{ gridColumn: index + 2 }}
+            className="text-center"
+          >
+            {day.shortName}
+          </div>
+        ))}
+        {/* Times */}
+        {TIME_HOURS.map((hour, index) => (
+          <div
+            key={index}
+            style={{ gridColumn: 1, gridRow: `${index * 12 + 2} / span 12` }}
+            className="text-right pr-3 tabular-nums py-2"
+          >
+            {hour}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
---
+++ utils/date/display.ts
@@ -0,0 +1,14 @@
+export const DAYS = [
+  { name: "Sunday", shortName: "Sun", singleChar: "S" },
+  { name: "Monday", shortName: "Mon", singleChar: "M" },
+  { name: "Tuesday", shortName: "Tue", singleChar: "T" },
+  { name: "Wednesday", shortName: "Wed", singleChar: "W" },
+  { name: "Thursday", shortName: "Thu", singleChar: "R" },
+  { name: "Friday", shortName: "Fri", singleChar: "F" },
+  { name: "Saturday", shortName: "Sat", singleChar: "S" },
+];
+export const WEEK_DAYS = DAYS.slice(1, 6);
+
+export const TIME_HOURS = [
+  7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+];
