ca3c3a5e14fd1f63a9601f07f5c61c7509d4403f
ninest <parth.kabra@gmail.com>
2023-02-17 14:29:22 -0500

    Create config utils

---
+++ utils/event/conflict.ts
@@ -0,0 +1,17 @@
+import { CalendarEvent } from "@/types";
+
+// Return a list of a list of events that have conflicts
+export const getConflictList = (events: CalendarEvent) => {};
+
+export const isConflict = (eventA: CalendarEvent, eventB: CalendarEvent) => {
+  if (eventA.day !== eventB.day) return false;
+
+  const startA = eventA.startTime.hour * 60 + eventA.startTime.minute;
+  const endA = eventA.endTime.hour * 60 + eventA.endTime.minute;
+  const startB = eventB.startTime.hour * 60 + eventB.startTime.minute;
+  const endB = eventB.endTime.hour * 60 + eventB.endTime.minute;
+
+  // return(
+  //   startA > startB
+  // )
+};
