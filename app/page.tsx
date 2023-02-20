90021df0455a4b16519178a08f444f1cf11f7b5f
ninest <parth.kabra@gmail.com>
2023-02-19 19:13:48 -0500

    Add buttons, home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <main>Hello</main>;
+import { Button } from "@/components/button";
+import { Plus } from "lucide-react";
+
+export default function HomePage() {
+  return (
+    <main className="">
+      <header className="border-b p-5 flex justify-between items-center">
+        <h1 className="text-xl font-bold">Course Planner</h1>
+        <Button intent={"primary"}>
+          <Plus className="md:mr-2 w-5" />
+          <span className="hidden md:block">New Plan</span>
+        </Button>
+      </header>
+    </main>
+  );
 }
---
+++ components/button.tsx
@@ -0,0 +1,63 @@
+import { cva, VariantProps } from "class-variance-authority";
+import clsx from "clsx";
+import Link from "next/link";
+import { ComponentProps } from "react";
+
+const buttonStyles = cva(
+  ["inline-flex font-medium tracking-wide", "rounded-lg", "border-4"],
+  {
+    variants: {
+      intent: {
+        primary: ["bg-indigo-600 text-gray-200 hover:bg-indigo-500"],
+        secondary: ["bg-gray-200 text-gray-800 hover:bg-gray-300"],
+      },
+      outline: {
+        true: "",
+        false: "border-transparent",
+      },
+      size: {
+        sm: ["text-sm py-0.5 px-1"],
+        default: ["text-base py-1 px-3"],
+      },
+    },
+    compoundVariants: [
+      {
+        intent: "primary",
+        outline: true,
+        class: [
+          "bg-transparent text-indigo-600 border-indigo-600 hover:bg-gray-100",
+        ],
+      },
+    ],
+    defaultVariants: {
+      intent: "secondary",
+      size: "default",
+      outline: false,
+    },
+  }
+);
+
+type ButtonProps = VariantProps<typeof buttonStyles> &
+  ComponentProps<"button"> & { href?: string };
+export const Button = ({
+  intent,
+  outline,
+  size,
+  href,
+  children,
+  className,
+  ...props
+}: ButtonProps) => {
+  const classes = clsx(className, buttonStyles({ intent, outline, size }));
+  if (href)
+    return (
+      <Link href={href} className={classes}>
+        {children}
+      </Link>
+    );
+  return (
+    <button className={classes} {...props}>
+      {children}
+    </button>
+  );
+};
